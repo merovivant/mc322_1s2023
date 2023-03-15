85a41142926ef5000ff3c26a60da672e771baa2c
Jonatas de Sousa Santos <j225334@dac.unicamp.br>
2023-03-14 22:37:52 -0300

    Criando as classes necessárias com seus métodos get/set

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Cliente cliente = new Cliente();
+        cliente.setCpf("539.224.108-55 OI EU SOU O JONATAS10");
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/Sinistro.java
@@ -0,0 +1,29 @@
+public class Sinistro {
+    private int id;
+    private String data;
+    private String endereco;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+    
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
